75381eef636db18d4344b450e49549b5657a1e09
liumiao2 <liumiao2@4399.com>
2025-12-02 10:21:36 +0800

    [add] 更多的提交前缀

--- src/commit/cm-content.ts
+++ src/commit/cm-content.ts
@@ -7,6 +7,17 @@ const CONTENT_ID = {
   refactor: 'refactor',
   fix: 'fix',
   modify: 'modify',
+  version: 'version',
+  style: 'style',
+  test: 'test',
+  docs: 'docs',
+  chore: 'chore',
+  perf: 'perf',
+  build: 'build',
+  ci: 'ci',
+  revert: 'revert',
+  feat: 'feat',
+  optimize: 'optimize',
 };
 
 const CONTENT_TYPE_MAP: TypeMap = {
@@ -15,6 +26,17 @@ const CONTENT_TYPE_MAP: TypeMap = {
   [CONTENT_ID.refactor]: ['[refactor]', '重构', 'refactor '],
   [CONTENT_ID.fix]: ['[fix]', '修复', 'fix '],
   [CONTENT_ID.modify]: ['[modify]', '修改', 'modify '],
+  [CONTENT_ID.style]: ['[style]', '样式', 'style '],
+  [CONTENT_ID.test]: ['[test]', '测试', 'test '],
+  [CONTENT_ID.docs]: ['[docs]', '文档', 'docs '],
+  [CONTENT_ID.chore]: ['[chore]', '杂项', 'chore '],
+  [CONTENT_ID.perf]: ['[perf]', '性能', 'perf '],
+  [CONTENT_ID.build]: ['[build]', '构建', 'build '],
+  [CONTENT_ID.ci]: ['[ci]', '持续集成', 'ci '],
+  [CONTENT_ID.revert]: ['[revert]', '回滚', 'revert '],
+  [CONTENT_ID.version]: ['[version]', '版本', 'version '],
+  [CONTENT_ID.feat]: ['[feat]', '功能', 'feat '],
+  [CONTENT_ID.optimize]: ['[optimize]', '优化', 'optimize '],
 };
 
 export function cmContent(br: Branches, content: string): { commitMessage: string; hash: string } | null {
